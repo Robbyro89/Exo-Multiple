5c712dd37270fe6120f8279b57a3206f144a1890
fabien <gallery.laroche.id@gmail.com>
2018-01-15 23:55:17 +0800

    Exo2 update

--- Exo2.rb
+++ Exo2.rb
@@ -1,7 +1,27 @@
-def chiffre_de_cesar(string, shift = 5)
-    alphabet   = Array('a'..'z') + Array('A'..'Z')
-    encrypter  = Hash[alphabet.zip(alphabet.rotate(shift))]
-    string.chars.map { |c| encrypter.fetch(c, " ") }
-end
+def chiffre_de_cesar(string, key)
 
-puts chiffre_de_cesar("What a string").join + "!"
+  key = key%26
+
+  string.each_byte do |letter|
+    if(letter >= 97) && (letter <= 122)
+      letter = letter + key
+        if letter > 122
+          print (letter - 26).chr
+        else
+          print letter.chr
+        end
+
+    elsif(letter >=65) && (letter <= 90)
+      letter += key
+      if (letter > 90)
+        print (letter - 26).chr
+      else
+        print letter.chr
+      end
+
+    else
+      print letter.chr
+    end
+  end
+end
+chiffre_de_cesar("What a string!", 5)
