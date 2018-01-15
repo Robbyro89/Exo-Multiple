f625e12d14f5e99e6c858a7c85f7b067c29f8889
fabien <gallery.laroche.id@gmail.com>
2018-01-16 00:06:13 +0800

    Exo3_bis fait

---
+++ Exo3_bis.rb
@@ -0,0 +1,85 @@
+def trader_du_lundi(prix)
+
+x = prix.length-1
+
+tab = []
+
+grostab = []
+
+
+for i in (0..x)
+	string =  "jour #{i}   "
+			for a in (0..i)
+			diff = prix [i]-prix[a]
+			tab << diff
+			end
+
+	tab << i
+	grostab << tab
+	tab = []
+end
+
+highday = grostab.max { |a, b| a.max <=> b.max }
+highdaybis = highday[0...-1]
+
+
+benef = highdaybis.max
+jourachat = highdaybis.index(highdaybis.max)
+jourvente = highday.last
+
+puts "[#{jourachat} ; #{jourvente}]  prix achat : #{prix[jourachat]} $  prix vente : #{prix[jourvente]} $ bénéfice : #{benef} $"
+
+end
+
+
+jour_1 = { :GOO => 15, :MMM => 14, :ADBE => 12, :EA=> 13, :BA => 8, :KO => 10, :XOM => 20, :GPS => 7, :MCD => 11, :DIS => 15, :CRM => 6, :JNJ => 10 }
+
+jour_2 = { :GOO => 8, :MMM => 20, :ADBE => 3, :EA=> 10, :BA => 5, :KO => 19, :XOM => 12, :GPS => 6, :MCD => 15, :DIS => 9, :CRM => 10, :JNJ => 17 }
+
+jour_3 = { :GOO => 3, :MMM => 8, :ADBE => 15, :EA=> 5, :BA => 10, :KO => 5, :XOM => 15, :GPS => 13, :MCD => 10, :DIS => 18, :CRM => 5, :JNJ => 14 }
+
+jour_4 = { :GOO => 17, :MMM => 3, :ADBE => 6, :EA=> 9, :BA => 15, :KO => 6, :XOM => 8, :GPS => 1, :MCD => 10, :DIS => 15, :CRM => 18, :JNJ => 3 }
+
+jour_5 = { :GOO => 8, :MMM => 18, :ADBE => 4, :EA=> 6, :BA => 15, :KO => 18, :XOM => 3, :GPS => 12, :MCD => 19, :DIS => 3, :CRM => 7, :JNJ => 9 }
+
+jour_6 = { :GOO => 10, :MMM => 12, :ADBE => 8, :EA=> 3, :BA => 18, :KO => 20, :XOM => 5, :GPS => 11, :MCD => 3, :DIS => 9, :CRM => 8, :JNJ => 15 }
+
+jour_7 = { :GOO => 17, :MMM => 14, :ADBE => 2, :EA=> 17, :BA => 7, :KO => 13, :XOM => 1, :GPS => 15, :MCD => 15, :DIS => 10, :CRM => 9, :JNJ => 17 }
+
+
+
+
+
+tab = [{ :GOO => 15, :MMM => 14, :ADBE => 12, :EA=> 13, :BA => 8, :KO => 10, :XOM => 20, :GPS => 7, :MCD => 11, :DIS => 15, :CRM => 6, :JNJ => 10 }, 
+
+	{ :GOO => 8, :MMM => 20, :ADBE => 3, :EA=> 10, :BA => 5, :KO => 19, :XOM => 12, :GPS => 6, :MCD => 15, :DIS => 9, :CRM => 10, :JNJ => 17 },
+
+	{ :GOO => 3, :MMM => 8, :ADBE => 15, :EA=> 5, :BA => 10, :KO => 5, :XOM => 15, :GPS => 13, :MCD => 10, :DIS => 18, :CRM => 5, :JNJ => 14 },
+
+	{ :GOO => 17, :MMM => 3, :ADBE => 6, :EA=> 9, :BA => 15, :KO => 6, :XOM => 8, :GPS => 1, :MCD => 10, :DIS => 15, :CRM => 18, :JNJ => 3 },
+
+	{ :GOO => 8, :MMM => 18, :ADBE => 4, :EA=> 6, :BA => 15, :KO => 18, :XOM => 3, :GPS => 12, :MCD => 19, :DIS => 3, :CRM => 7, :JNJ => 9 },
+
+	{ :GOO => 10, :MMM => 12, :ADBE => 8, :EA=> 3, :BA => 18, :KO => 20, :XOM => 5, :GPS => 11, :MCD => 3, :DIS => 9, :CRM => 8, :JNJ => 15 },
+
+	{ :GOO => 17, :MMM => 14, :ADBE => 2, :EA=> 17, :BA => 7, :KO => 13, :XOM => 1, :GPS => 15, :MCD => 15, :DIS => 10, :CRM => 9, :JNJ => 17 }]
+
+
+
+
+for i in (0..11)
+tableau = []
+entreprise = " "
+tab.each do |hashes|
+	Hash[*hashes.to_a.at(i)].each_value{
+		|value| tableau << value }
+	entreprise = hashes.keys[i]
+end
+puts " "
+puts entreprise
+print tableau
+puts " "
+trader_du_lundi(tableau)
+tableau = [0]
+entreprise = " "	
+end
